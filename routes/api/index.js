4d52cea8f55a45777117cfd8dc212e641a0fdaaf
cheddar <nowaczewskijack@gmail.com>
2022-07-08 20:12:40 -0400

    add post class model

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
-const router = require("express").Router();
-const userRoutes = require("./user-routes.js");
+const router = require('express').Router();
 
-router.use("/users", userRoutes);
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const routes = require("./routes");
-const sequelize = require("./config/connection");
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.unsubscribe(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
 
-// turn pon connection to db and server
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now Listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
