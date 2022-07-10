43c8b313a776dc34df3817c20687b14add8df800
cheddar <nowaczewskijack@gmail.com>
2022-07-10 12:29:32 -0400

    connect to jawsdv

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,22 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // create connectio to the database, pass in the Mysql information  for username and password
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_LOCAL_HOST || "localhost",
-    dialect: "mysql",
-    port: 3306,
-    // might have to remove due to mysql
-    // socketPath: "/tmp/mysql.sock",
-  }
-);
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_LOCAL_HOST || "localhost",
+      dialect: "mysql",
+      port: 3306,
+      // might have to remove due to mysql
+      // socketPath: "/tmp/mysql.sock",
+    }
+  );
+}
 module.exports = sequelize;
