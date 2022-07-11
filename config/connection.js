ff176bbb5786c29d04e165f2dc14240e43c9c4c6
cheddar <nowaczewskijack@gmail.com>
2022-07-11 19:45:55 -0400

    final push before mvc

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 // import the sequilize constructor from the library
 const Sequelize = require("sequelize");
 
+const Sequelize = require("sequelize");
+
 require("dotenv").config();
 
-// create connectio to the database, pass in the Mysql information  for username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -12,14 +13,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: process.env.DB_LOCAL_HOST || "localhost",
       dialect: "mysql",
       port: 3306,
-      // might have to remove due to mysql
-      // socketPath: "/tmp/mysql.sock",
     }
   );
 }
+
 module.exports = sequelize;
